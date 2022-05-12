85a4ab9696e5c1084d2121081f0b68e218e09dbc
VKlapan <vladimir.klapan@gmail.com>
2022-05-12 17:30:03 +0300

    Fixing size 786-1280 of reviews

--- src/js/modal-hero.js
+++ src/js/modal-hero.js
@@ -1,14 +1,16 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector("[data-modal-open]"),
-    closeModalBtn: document.querySelector("[data-modal-close]"),
-    modal: document.querySelector("[data-modal]"),
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+    header: document.querySelector('[data-header]'),
   };
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
-    refs.modal.classList.toggle("is-hidden-hero");
+    refs.modal.classList.toggle('is-hidden-hero');
+    refs.header.classList.toggle('is-z-index-header');
   }
 })();
